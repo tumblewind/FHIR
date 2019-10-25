26377f306d06cb82b7130e2a29e680c3e936e99b
Paul Bastide <pbastide@us.ibm.com>
2019-10-24 21:45:15 -0400

    init site
    Signed-off-by: Paul Bastide <pbastide@us.ibm.com>

--- src/gatsby-theme-carbon/components/Header.js
+++ src/gatsby-theme-carbon/components/Header.js
@@ -2,7 +2,7 @@ import React from 'react';
 import Header from 'gatsby-theme-carbon/src/components/Header';
 
 const CustomHeader = props => (
-  <Header {...props} href="https://ibm.github.io/FHIR/">
+  <Header {...props} to="/FHIR">
     <span>IBM</span>&nbsp;FHIR&reg;&nbsp;Server
   </Header>
 );
