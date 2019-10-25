33ed9e4ed4e85560994b31cfeb635b64b33e427c
Paul Bastide <pbastide@us.ibm.com>
2019-10-25 07:38:21 -0400

    init site
    Signed-off-by: Paul Bastide <pbastide@us.ibm.com>

--- src/gatsby-theme-carbon/components/Header.js
+++ src/gatsby-theme-carbon/components/Header.js
@@ -3,7 +3,7 @@ import Header from 'gatsby-theme-carbon/src/components/Header';
 
 const CustomHeader = props => (
   <Header {...props} href="/FHIR">
-    <a href="/FHIR"><span>IBM</span>&nbsp;FHIR&reg;&nbsp;Server</a>
+    <a href="/FHIR" class="bx--header__name"><span>IBM</span>&nbsp;FHIR&reg;&nbsp;Server</a>
   </Header>
 );
 
