213b0c968807c9b0f2b10ad70ac9e905b34df4ac
Paul Bastide <pbastide@us.ibm.com>
2019-10-29 09:48:25 -0400

    init site
    Signed-off-by: Paul Bastide <pbastide@us.ibm.com>

--- src/gatsby-theme-carbon/components/Footer.js
+++ src/gatsby-theme-carbon/components/Footer.js
@@ -7,7 +7,7 @@ const Content = () => (
       <a href="https://travis-ci.com/IBM/FHIR"><img src="https://travis-ci.com/IBM/FHIR.svg?branch=master" alt="Build Status"></img></a>
     </p>
     <p>
-      The IBM FHIR Server is licensed under the Apache 2.0 license. Full license text is available at <a href="/LICENSE">LICENSE</a>.
+      The IBM FHIR Server is licensed under the Apache 2.0 license. Full license text is available at <a href="https://ibm.github.io/FHIR/LICENSE">LICENSE</a>.
     </p>
     <p>
       <a href="https://www.hl7.org/fhir/index.html">
