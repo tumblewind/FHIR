8d383fb143232720424cdb56e4efa9a2fea44bb7
Paul Bastide <pbastide@us.ibm.com>
2020-03-02 12:36:58 -0500

    e2e windows tests report success even when e2e tests fail #698
    Signed-off-by: Paul Bastide <pbastide@us.ibm.com>

--- build/tests.ps1
+++ build/tests.ps1
@@ -3,17 +3,11 @@
 # 
 # SPDX-License-Identifier: Apache-2.0
 ###############################################################################
-
+$ErrorActionPreference = 'Stop'
 Write-Host 'Performing Tests'
-& mvn -B test -DskipTests=false -f .\fhir-server-test\pom.xml -DskipWebSocketTest=true --log-file out.log --no-transfer-progress
+$PROC = Start-Process -FilePath "mvn.cmd" -ArgumentList "-B test -DskipTests=false -f .\fhir-server-test\pom.xml -DskipWebSocketTest=true --no-transfer-progress" -PassThru
+Wait-Process -InputObject $PROC
 
-If ( (Get-Content out.log -Tail 10 | Select-String -pattern "BUILD SUCCESS").Length > 0) {
-    Write-Host 'Performing Tests - Success'
-    exit 0
-} else { 
-    Write-Host 'Performing Tests - Fail'
-    cat out.log
-    exit -1
-}
-
+Write-Host "Exit Code is " $PROC.ExitCode
+exit $PROC.ExitCode
 # End of Script
